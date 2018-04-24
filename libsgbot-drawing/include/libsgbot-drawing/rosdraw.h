19105ed06e7c1fea02182ff81388dbf11a672576
cyberniklee <cyberniklee@gmail.com>
2018-04-24 18:21:16 +0800

    fix errors

--- libsgbot-drawing/include/libsgbot-drawing/rosdraw.h
+++ libsgbot-drawing/include/libsgbot-drawing/rosdraw.h
@@ -9,6 +9,7 @@
 #include <type/pose2d.h>
 #include <type/point2d.h>
 #include <linear-algebra/matrix.h>
+#include <common/draw.h>
 //ros
 #include <ros/node_handle.h>
 #include <sensor_msgs/LaserScan.h>
@@ -18,16 +19,17 @@
 #include <geometry_msgs/PointStamped.h>
 #include <tf/tf.h>
 
-class RosDraw {
+class RosDraw : public sgbot::DrawCallbacks {
 public:
-  RosDraw(ros::NodeHandle nh = ros::NodeHandle("~")) : nh_(nh)
+  RosDraw(ros::NodeHandle nh = ros::NodeHandle("~"))
+    : nh_(nh)
   {
-    scan_publisher = nh_.advertise<sensor_msgs::LaserScan>("LaserScan",1);
+    scan_publisher_ = nh_.advertise<sensor_msgs::LaserScan>("LaserScan",1);
     //rviz can only subscribe PoseStamped
-    pose_publisher = nh_.advertise<geometry_msgs::PoseStamped>("Pose",1);
+    pose_publisher_ = nh_.advertise<geometry_msgs::PoseStamped>("Pose",1);
     //the same
-    pose_covariance_publisher = nh_.advertise<geometry_msgs::PoseWithCovarianceStamped>("PoseWithCovariance",1);
-    point_publisher = nh_.advertise<geometry_msgs::PointStamped>("Point",1);
+    pose_covariance_publisher_ = nh_.advertise<geometry_msgs::PoseWithCovarianceStamped>("PoseWithCovariance",1);
+    point_publisher_ = nh_.advertise<geometry_msgs::PointStamped>("Point",1);
   }
 
   virtual ~RosDraw()
@@ -36,98 +38,96 @@ public:
     reset();
   }
 
-  void displayScan(const sgbot::sensor::Lidar2D& scan)
+  virtual void drawScan(const sgbot::sensor::Lidar2D& scan)
   {
     int num_readings = scan.getCount();
     int laser_frequency = 40;
 
-    laserScan.angle_min = -1.57;
-    laserScan.angle_max = 1.57;
-    laserScan.angle_increment = 3.14 / num_readings;
-    laserScan.time_increment = (1 / laser_frequency) / (num_readings);
-    laserScan.range_min = 0.0;
-    laserScan.range_max = 100.0;
+    laser_scan_.angle_min = -1.57;
+    laser_scan_.angle_max = 1.57;
+    laser_scan_.angle_increment = 3.14 / num_readings;
+    laser_scan_.time_increment = (1 / laser_frequency) / (num_readings);
+    laser_scan_.range_min = 0.0;
+    laser_scan_.range_max = 100.0;
 
-    laserScan.ranges.resize(scan.getCount());
+    laser_scan_.ranges.resize(scan.getCount());
     for (int i = 0; i < num_readings; ++i)
     {
       //does Lidar2d need a distance method?
-      laserScan.ranges[i] = sgbot::distance(scan.getOrigin(), scan.getPoint(i));
+      laser_scan_.ranges[i] = sgbot::distance(scan.getOrigin(), scan.getPoint(i));
     }
-    laserScan.header.frame_id = "map";
+    laser_scan_.header.frame_id = "map";
   }
 
-  void displayPose(const sgbot::Pose2D& pose)
+  virtual void drawPose(const sgbot::Pose2D& pose)
   {
-    geo_pose.pose.position.x = pose.x();
-    geo_pose.pose.position.y = pose.y();
-    geo_pose.pose.position.z = 0.0;
-    geo_pose.pose.orientation = tf::createQuaternionMsgFromYaw(pose.theta());
+    geo_pose_.pose.position.x = pose.x();
+    geo_pose_.pose.position.y = pose.y();
+    geo_pose_.pose.position.z = 0.0;
+    geo_pose_.pose.orientation = tf::createQuaternionMsgFromYaw(pose.theta());
 
-    geo_pose.header.frame_id = "map";
+    geo_pose_.header.frame_id = "map";
   }
 
-  void displayPoseWithCovariance(const sgbot::Pose2D& pose,const sgbot::la::Matrix<float,3,3>& covariance)
+  virtual void drawPoseWithCovariance(const sgbot::Pose2D& pose,const sgbot::la::Matrix<float,3,3>& covariance)
   {
     //initial pose
-    poseWithCovarianceStamped.pose.pose.position.x = pose.x();
-    poseWithCovarianceStamped.pose.pose.position.y = pose.y();
-    poseWithCovarianceStamped.pose.pose.position.z = 0.0;
-    poseWithCovarianceStamped.pose.pose.orientation = tf::createQuaternionMsgFromYaw(pose.theta());
+    pose_with_covariance_.pose.pose.position.x = pose.x();
+    pose_with_covariance_.pose.pose.position.y = pose.y();
+    pose_with_covariance_.pose.pose.position.z = 0.0;
+    pose_with_covariance_.pose.pose.orientation = tf::createQuaternionMsgFromYaw(pose.theta());
     //initial covariance
-    poseWithCovarianceStamped.pose.covariance[0] = covariance(0,0);//x and x
-    poseWithCovarianceStamped.pose.covariance[1] = covariance(0,1);//x and y
-    poseWithCovarianceStamped.pose.covariance[6] = covariance(1,0);//y and x
-    poseWithCovarianceStamped.pose.covariance[6 + 1] = covariance(1,1);//y and y
+    pose_with_covariance_.pose.covariance[0] = covariance(0, 0);//x and x
+    pose_with_covariance_.pose.covariance[1] = covariance(0, 1);//x and y
+    pose_with_covariance_.pose.covariance[6] = covariance(1, 0);//y and x
+    pose_with_covariance_.pose.covariance[6 + 1] = covariance(1, 1);//y and y
 
     //R,P,Y
-    poseWithCovarianceStamped.pose.covariance[5] = covariance(0,2);//x and theta
-    poseWithCovarianceStamped.pose.covariance[11] = covariance(1,2);//y and theta
-    poseWithCovarianceStamped.pose.covariance[30] = covariance(2,0);//theta and x
-    poseWithCovarianceStamped.pose.covariance[31] = covariance(2,1);//theta and y
-    poseWithCovarianceStamped.pose.covariance[35] = covariance(2,2);//theta and theta
+    pose_with_covariance_.pose.covariance[5] = covariance(0, 2);//x and theta
+    pose_with_covariance_.pose.covariance[11] = covariance(1, 2);//y and theta
+    pose_with_covariance_.pose.covariance[30] = covariance(2, 0);//theta and x
+    pose_with_covariance_.pose.covariance[31] = covariance(2, 1);//theta and y
+    pose_with_covariance_.pose.covariance[35] = covariance(2, 2);//theta and theta
 
-    poseWithCovarianceStamped.header.frame_id = "map";
-
+    pose_with_covariance_.header.frame_id = "map";
   }
 
-  void displayPoint(const sgbot::Point2D& point)
+  virtual void drawPoint(const sgbot::Point2D& point)
   {
-    pointStamped.point.x = point.x();
-    pointStamped.point.y = point.y();
+    point_stamp_.point.x = point.x();
+    point_stamp_.point.y = point.y();
 
-    pointStamped.header.frame_id = "map";
+    point_stamp_.header.frame_id = "map";
   }
 
-  void update()
+  virtual void update()
   {
-    scan_publisher.publish(laserScan);
-    pose_publisher.publish(geo_pose);
-    pose_covariance_publisher.publish(poseWithCovarianceStamped);
-    point_publisher.publish(pointStamped);
+    scan_publisher_.publish(laser_scan_);
+    pose_publisher_.publish(geo_pose_);
+    pose_covariance_publisher_.publish(pose_with_covariance_);
+    point_publisher_.publish(point_stamp_);
   }
 
-  void reset()
+  virtual void reset()
   {
-    scan_publisher.publish(sensor_msgs::LaserScan());
-    pose_publisher.publish(geometry_msgs::PoseStamped());
-    pose_covariance_publisher.publish(geometry_msgs::PoseWithCovarianceStamped());
-    point_publisher.publish(geometry_msgs::PointStamped());
+    scan_publisher_.publish(sensor_msgs::LaserScan());
+    pose_publisher_.publish(geometry_msgs::PoseStamped());
+    pose_covariance_publisher_.publish(geometry_msgs::PoseWithCovarianceStamped());
+    point_publisher_.publish(geometry_msgs::PointStamped());
   }
 private:
-  ros::Publisher scan_publisher;
-  ros::Publisher pose_publisher;
-  ros::Publisher pose_covariance_publisher;
-  ros::Publisher point_publisher;
+  ros::Publisher scan_publisher_;
+  ros::Publisher pose_publisher_;
+  ros::Publisher pose_covariance_publisher_;
+  ros::Publisher point_publisher_;
   //const std::string scan_topic_name,pose_topic_name,pose_covariance_topic_name,point_topic_name;
   ros::NodeHandle nh_;
 
-  sensor_msgs::LaserScan laserScan;
-  geometry_msgs::PoseStamped geo_pose;
-  geometry_msgs::PoseWithCovarianceStamped poseWithCovarianceStamped;
-  geometry_msgs::PointStamped pointStamped;
-
+  sensor_msgs::LaserScan laser_scan_;
+  geometry_msgs::PoseStamped geo_pose_;
+  geometry_msgs::PoseWithCovarianceStamped pose_with_covariance_;
+  geometry_msgs::PointStamped point_stamp_;
 };
 
 
-#endif _ROS_DRAW_H_
+#endif  // _ROS_DRAW_H_
--- libsgbot-hector/src/libsgbot-hector-node.cpp
+++ libsgbot-hector/src/libsgbot-hector-node.cpp
@@ -1,9 +1,134 @@
 #include <common/draw.h>
+#include <sensor/lidar2d.h>
+#include <slam/hector/mapping.h>
 
+#include <ros/init.h>
+#include <laser_geometry/laser_geometry.h>
+#include <nav_msgs/GetMap.h>
+
+#include <libsgbot-drawing/rosdraw.h>
+
+sgbot::DrawCallbacks* rosdraw = NULL;
+sgbot::slam::hector::HectorMapping* mapping = NULL;
+
+int last_map_update = 0;
+
+void initialDrawing()
+{
+  sgbot::draw = new sgbot::Draw();
+  rosdraw = new RosDraw();
+
+  rosdraw->reset();
+
+  sgbot::draw->setCallbacks(rosdraw);
+}
+
+void finishDrawing()
+{
+  delete sgbot::draw;
+  delete rosdraw;
+}
+
+geometry_msgs::PoseWithCovarianceStamped getPoseWithCovariance(const sgbot::Pose2D& pose, const sgbot::la::Matrix<float, 3, 3>& cov, const ros::Time& stamp, const std::string frame_id)
+{
+  geometry_msgs::PoseWithCovarianceStamped result;
+
+  result.header.stamp = stamp;
+  result.header.frame_id = frame_id;
+
+  result.pose.pose.position.x = pose.x();
+  result.pose.pose.position.y = pose.y();
+  result.pose.pose.orientation.w = cos(pose.theta() * 0.5f);
+  result.pose.pose.orientation.z = sin(pose.theta() * 0.5f);
+
+  result.pose.covariance[0] = cov(0, 0);
+  result.pose.covariance[7] = cov(1, 1);
+  result.pose.covariance[35] = cov(2, 2);
+  result.pose.covariance[1] = cov(0, 1);
+  result.pose.covariance[6] = cov(0, 1);
+  result.pose.covariance[5] = cov(0, 2);
+  result.pose.covariance[30] = cov(0, 2);
+  result.pose.covariance[11] = cov(1, 2);
+  result.pose.covariance[31] = cov(1, 2);
+
+  return result;
+}
+
+nav_msgs::GetMap::Response getMap(sgbot::Map2D& map2d, const ros::Time& stamp, const std::string frame_id)
+{
+  nav_msgs::GetMap::Response map;
+  // TODO: set origin
+
+  map.map.info.resolution = map2d.getResolution();
+  map.map.info.width = map2d.getWidth();
+  map.map.info.height = map2d.getHeight();
+  map.map.header.frame_id = frame_id;
+  map.map.header.stamp = stamp;
+
+  map.map.data.resize(map.map.info.width * map.map.info.height);
+
+  for(int x = 0; x < map.map.info.width; ++x)
+  {
+    for(int y = 0; y < map.map.info.height; ++y)
+    {
+      int val = -1;
+      if(map2d.isKnown(x, y))
+      {
+        val = 0;
+      }
+      else if(map2d.isEdge(x, y))
+      {
+        val = 100;
+      }
+    }
+  }
+
+  return map;
+}
+
+void scanCallback(const sensor_msgs::LaserScan& scan)
+{
+  ros::WallTime start_time = ros::WallTime::now();
+
+  sgbot::sensor::Lidar2D laser;
+
+  laser.clear();
+
+  sgbot::Point2D origin;
+  origin.x() = 0.0f;
+  origin.y() = 0.0f;
+
+  laser.setOrigin(origin);
+
+  float angle = scan.angle_min;
+
+  for(int i = 0; i < scan.ranges.size(); ++i)
+  {
+    float dist = scan.ranges[i];
+    if((dist > scan.range_min) && (dist < (scan.range_max - 0.1f)))
+    {
+      laser.addBeam(angle, dist);
+    }
+    angle += scan.angle_increment;
+  }
+
+  mapping->updateByScan(laser);
+}
 
 int main(int argc, char** argv)
 {
-  sgbot::draw = new sgbot::Draw();
+  initialDrawing();
+
+  mapping = new sgbot::slam::hector::HectorMapping();
+
+  // init ros node
+  ros::init(argc, argv, "libsgbot_hector_node");
+  ros::NodeHandle node;
+  ros::Subscriber scan_sub = node.subscribe("scan", 5, &scanCallback);
+
+  ros::spin();
+
+  finishDrawing();
 
   return 0;
 }
